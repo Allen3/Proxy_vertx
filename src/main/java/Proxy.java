f3c2f7959f4f06f7bbe0cdec6a07c12f681eb9a6
Allen3 <Allen.3.ysj@gmail.com>
2015-05-08 09:50:17 +0800

    HTTP proxy finished. working on HTTPS.

--- src/main/java/Proxy.java
+++ src/main/java/Proxy.java
@@ -1,7 +1,10 @@
 import io.netty.handler.codec.http.HttpRequest;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.*;
+import io.vertx.core.streams.Pump;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,15 +15,12 @@ import java.io.IOException;
  * Created by allen on 4/29/15.
  */
 public class Proxy extends AbstractVerticle {
-    private String fileName = "test";
-    private int fileSuf = 0;
 
     @Override
     public void start() throws Exception {
         HttpClient client = vertx.createHttpClient(new HttpClientOptions());
 
         vertx.createHttpServer(new HttpServerOptions().setPort(8080)).requestHandler(req -> {
-            System.out.println("Proxying request: " + req.uri());
 
             String host = req.headers().get("host");
             String hostAddress = host;
@@ -30,59 +30,46 @@ public class Proxy extends AbstractVerticle {
                 hostPort = Integer.valueOf(host.substring(host.indexOf(":") + 1));
             }
 
-            System.out.println("hostAddress = " + hostAddress + " | hostPort = " + hostPort);
-
-            for (String name : req.headers().names()) {
-                System.out.println("name: " + name);
-            }
-
-
             HttpClientRequest c_req = client.request(req.method(), hostPort, hostAddress, req.uri(), c_res -> {
-                fileSuf++;
-                System.out.println("Proxying response: " + c_res.statusCode());
-
-                for (String name : c_res.headers().names()) {
-                    System.out.println("Resp name: " + name);
-                }
-
 
                 req.response().setStatusCode(c_res.statusCode());
+                req.response().setStatusMessage(c_res.statusMessage());
                 req.response().headers().setAll(c_res.headers());
+                if (c_res.headers().get("Content-Length") == null) {
+                    req.response().setChunked(true);
+                }
 
-                c_res.handler(data -> {
-                    System.out.println("Proxying response body: " + data.toString("ISO-8859-1"));
-                    req.response().write(data);
-                    /*
-                    File file = new File(fileName + fileSuf+".png");
-                    try {
-                        FileOutputStream fileOutputStream = new FileOutputStream(file);
-                        fileOutputStream.write(data.getBytes());
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    */
-                });
+                Pump target2Proxy = Pump.pump(c_res, req.response());
+                target2Proxy.start();
+
                 c_res.endHandler((v) -> req.response().end());
             });
-            if (req.method().equals(HttpMethod.POST) ||
-                    req.method().equals(HttpMethod.PUT)) {
+            if (req.method().equals(HttpMethod.POST) || req.method().equals(HttpMethod.PUT)) {
                 if (req.headers().get("Content-Length") == null) {
                     c_req.setChunked(true);
                 }
             }
-            
+
             c_req.headers().setAll(req.headers());
-            req.handler(data -> {
-                System.out.println("Proxying request body " + data.toString("ISO-8859-1"));
-                c_req.write(data);
-            });
+            Pump proxy2Target = Pump.pump(req, c_req);
+            proxy2Target.start();
+
             req.endHandler((v) -> c_req.end());
-        }).listen();
+        }).listen(httpServerAsyncResult -> {
+            if (httpServerAsyncResult.succeeded()) {
+                System.out.println("Binding succeed!");
+            } else {
+                System.out.println("Binding failed");
+                try {
+                    throw httpServerAsyncResult.cause();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
     }
 
-    public void main(String args[]) {
+    public static void main(String args[]) {
         Proxy proxy = new Proxy();
         Vertx.vertx().deployVerticle(proxy);
     }
